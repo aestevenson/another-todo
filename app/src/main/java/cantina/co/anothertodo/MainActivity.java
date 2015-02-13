3175ef69585b175fafd1ea5449755032014dad19
Aaron Stevenson <aaron@cantina.co>
2015-02-13 09:42:56 -0500

    Initial commit

---
+++ app/src/main/java/cantina/co/anothertodo/MainActivity.java
@@ -0,0 +1,20 @@
+package cantina.co.anothertodo;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by aaronstevenson on 2/12/15.
+ */
+public class MainActivity extends Activity {
+
+    @Override
+    public void onCreate(Bundle savedBundle) {
+        super.onCreate(savedBundle);
+
+        setContentView(R.layout.activity_main);
+
+    }
+
+
+}
